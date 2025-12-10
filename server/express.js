e511c893763637471a8ce065e46d43fc91b4cc2a
Francesca Mpolomena <mfrance2@my.centennialcollege.ca>
2025-12-10 13:03:52 -0500

    Fix static path for Render SPA routing

--- server/express.js
+++ server/express.js
@@ -1,3 +1,5 @@
+import path from "path";
+import { fileURLToPath } from "url";
 import taskRoutes from "./routes/task.routes.js";
 import express from "express";
 import bodyParser from "body-parser";
@@ -37,6 +39,29 @@ app.use(express.static(path.join(CURRENT_DIR, "client", "dist")));
 
 app.get(/.*/, (req, res) => {
   res.sendFile(path.join(CURRENT_DIR, "client", "dist", "index.html"));
+});
+
+// existing error handler...
+app.use((err, req, res, next) => {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({ error: err.name + ": " + err.message });
+  } else if (err) {
+    res.status(400).json({ error: err.name + ": " + err.message });
+    console.log(err);
+  }
+});
+
+// Find the *project root* (one level up from /server)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const ROOT_DIR = path.join(__dirname, "..");
+
+// Serve the built React files
+app.use(express.static(path.join(ROOT_DIR, "client", "dist")));
+
+// For any non-API route, send back index.html (SPA)
+app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(path.join(ROOT_DIR, "client", "dist", "index.html"));
 });
     
 export default app;
